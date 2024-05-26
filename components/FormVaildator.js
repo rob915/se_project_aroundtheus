b4501c3bc0ac47bd3c39037739f04a94fbd6e3f6
Robert_Rodriguez <rob915@gmail.com>
2024-05-26 08:51:46 -0700

    refactor: hancoded selector in enableSubmitButton method

--- components/FormVaildator.js
+++ components/FormVaildator.js
@@ -35,7 +35,7 @@ export default class FormValidator {
   }
 
   _enableSubmitButton() {
-    this._submitButton.classList.remove("modal__save-button_disabled");
+    this._submitButton.classList.remove(this._inactiveButtonClass);
     this._submitButton.disabled = false;
   }
 
