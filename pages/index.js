8fab47810d20943d7f3ccd325b349e9ed3c663cc
Robert_Rodriguez <rob915@gmail.com>
2024-05-26 00:29:51 -0700

    bug: corrected import statement for card class Card2.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Card from "../components/Card2.js";
+import Card from "../components/Card.js";
 import FormValidator from "../components/FormVaildator.js";
 
 // ! ||--------------------------------------------------------------------------------||
