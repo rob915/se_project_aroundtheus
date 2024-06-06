9cdb058a26d093181a718495a2601f05f51650cb
Robert_Rodriguez <rob915@gmail.com>
2024-06-05 21:34:57 -0700

    alternate Card.js  name to fix Save name

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Card from "../components/Card.js";
+import Card from "../components/Object.js";
 import FormValidator from "../components/FormVaildator.js";
 
 // ! ||--------------------------------------------------------------------------------||
